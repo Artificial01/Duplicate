7b5b96121624810fced5c479a33b3481d3bb1d45
Artificial01 <3372506727@qq.com>
2022-11-10 15:35:39 +0800

    lock is OK and update merge

--- main.c
+++ main.c
@@ -5,12 +5,10 @@
 #include "lock.h"
 
 int main(){
-
     char path[32] = "002";
-
     int choice;
     while(1){
-        printf("Input: ");
+        puts("INPUT 1(compress) 2(reverse compress) 3(abstract) 4(reverse abstract) 5(merge) 6(reverse merge) 7(lock) 8(reverse lock) TO CHANGE FOLDER/FILE NAMED 002");
         scanf("%d",&choice);
         if(choice==1){
             process_compress(path);
@@ -19,15 +17,17 @@ int main(){
         }else if(choice==3){
             process_abstract(path);
         }else if(choice==4){
-            printf("%s",process_reverse_abstract(path));
+            printf("%s\n",process_reverse_abstract(path));
         }else if(choice==5){
             process_merge(path);
         }else if(choice==6){
             process_reverse_merge(path);
+        }else if(choice==7){
+            process_lock(path,(byte *)"1234567812345678");
+        }else if(choice==8){
+            process_reverse_lock(path,(byte *)"1234567812345678");
         }else{
             break;
         }
     }
-
-    return 0;
 }
--- merge.h
+++ merge.h
@@ -30,8 +30,8 @@ void file_merge(){
     file_memory * file_memory_pointer = load_file(folder_path);
     merge_node * merge_node_pointer = (merge_node *)malloc(sizeof(merge_node));
     merge_node_pointer->merge_message.type = FILE_TYPE;
-    for(int i=0; i<=folder_stack[folder_stack_index]; i++){
-        merge_node_pointer->merge_message.path[i] = folder_path[i];
+    for(int i=folder_stack[0]+1; i<=folder_stack[folder_stack_index]; i++){
+        merge_node_pointer->merge_message.path[i-folder_stack[0]-1] = folder_path[i];
     }
     merge_node_pointer->merge_message.size = (long)sizeof(merge_message)+file_memory_pointer->size;
     merge_node_pointer->file_memory_pointer = file_memory_pointer;
@@ -43,8 +43,8 @@ void file_merge(){
 void folder_merge(){
     merge_node * merge_node_pointer = (merge_node *)malloc(sizeof(merge_node));
     merge_node_pointer->merge_message.type = FOLDER_TYPE;
-    for(int i=0; i<=folder_stack[folder_stack_index]; i++){
-        merge_node_pointer->merge_message.path[i] = folder_path[i];
+    for(int i=folder_stack[0]+1; i<=folder_stack[folder_stack_index]; i++){
+        merge_node_pointer->merge_message.path[i-folder_stack[0]-1] = folder_path[i];
     }
     merge_node_pointer->merge_message.size = sizeof(merge_message);
     merge_node_pointer->file_memory_pointer = NULL;
@@ -66,10 +66,16 @@ void folder_merge(){
 }
 
 void process_merge(char * path){
-    merge_node merge_node = {{},NULL,NULL};
+    merge_node merge_node;
+    merge_node.file_memory_pointer = NULL;
+    merge_node.next = NULL;
     merge_current_node_pointer = &merge_node;
     init_folder(path);
-    folder_merge();
+    if(identify_folder(path)){
+        folder_merge();
+    }else{
+        file_merge();
+    }
     file_memory * file_memory_pointer = (file_memory *)malloc(sizeof(file_memory));
     file_memory_pointer->size = 0;
     for(merge_current_node_pointer=merge_node.next; merge_current_node_pointer; merge_current_node_pointer=merge_current_node_pointer->next){
@@ -99,19 +105,31 @@ void process_merge(char * path){
 void process_reverse_merge(char * path){
     file_memory * file_memory_pointer = load_file(path);
     byte * pointer = (byte *)(file_memory_pointer->file);
-    merge_message buffer;
-    delete_file(path);
-    while(1){
-        buffer = *((merge_message *)pointer);
-        if(buffer.type==FOLDER_TYPE){
-            mkdir(buffer.path);
-        }else if(buffer.type==FILE_TYPE){
-            file_memory temporary_file_memory = {pointer+sizeof(merge_message),buffer.size-(long)sizeof(merge_message)};
-            store_file(buffer.path,&temporary_file_memory);
-        }else{
-            break;
-        }
+    remove(path);
+    init_folder(path);
+    merge_message buffer = *((merge_message *)pointer);
+    if(buffer.type==FOLDER_TYPE){
+        mkdir(path);
         pointer+=buffer.size;
+        while(1){
+            buffer = *((merge_message *)pointer);
+            if(buffer.type==FOLDER_TYPE){
+                enter_folder(buffer.path);
+                mkdir(folder_path);
+                exit_folder();
+            }else if(buffer.type==FILE_TYPE){
+                file_memory temporary_file_memory = {pointer+sizeof(merge_message),buffer.size-(long)sizeof(merge_message)};
+                enter_folder(buffer.path);
+                store_file(folder_path,&temporary_file_memory);
+                exit_folder();
+            }else{
+                break;
+            }
+            pointer+=buffer.size;
+        }
+    }else if(buffer.type==FILE_TYPE){
+        file_memory temporary_file_memory = {pointer+sizeof(merge_message),buffer.size-(long)sizeof(merge_message)};
+        store_file(path,&temporary_file_memory);
     }
     free_file_memory(file_memory_pointer);
 }
